3a08fe8f6665ce82e5a7ec976965ac32c1cfc89d
Denis Shimsirt <denis.shimshirt@gmail.com>
2024-03-06 16:22:10 +0300

    Made the layout of the swiper section.

--- gulp-html-scss-js/src/js/index.js
+++ gulp-html-scss-js/src/js/index.js
@@ -1,3 +1,26 @@
+import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: "auto",
+    // slidesPerView: 5,
+    spaceBetween: 24,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+  });
+  
+
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
