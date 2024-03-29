a9200eda75d878d464098a49dd2fe87e25078f39
Denis Shimsirt <denis.shimshirt@gmail.com>
2024-03-29 20:17:11 +0300

    Made mobile navigation.

--- gulp-html-scss-js/src/js/modules/mobile-nav.js
+++ gulp-html-scss-js/src/js/modules/mobile-nav.js
@@ -1,13 +1,28 @@
 function mobileNav() {
 	// Mobile nav button
-	const navBtn = document.querySelector('#mobile-nav-btn');
+	const navBtnOpen = document.querySelector('#mobile-nav-btn');
+	const navBtnClose = document.querySelector('#close-mobile-nav');
+	const mobileNavFade = document.querySelector('.mobile-nav-fade');
+
 	const nav = document.querySelector('.mobile-nav');
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
+	navBtnOpen.onclick = openMobileNav;
+
+	navBtnClose.onclick = closeMobileNav;
+	mobileNavFade.onclick = closeMobileNav;
+
+	function openMobileNav() {
+		nav.classList.add('mobile-nav--open');
+		mobileNavFade.classList.add('mobile-nav-fade--open');
 		document.body.classList.toggle('no-scroll');
-	};
+	}
+
+	function closeMobileNav() {
+		nav.classList.remove('mobile-nav--open');
+		mobileNavFade.classList.remove('mobile-nav-fade--open');
+		document.body.classList.toggle('no-scroll');
+	}
+
 }
 
 export default mobileNav;
